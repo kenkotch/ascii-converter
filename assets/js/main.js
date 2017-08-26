f3484658a96e8278abf9877e2148c0a98e4eef83
Ken Kotch <hello@ken.uno>
2017-08-26 16:10:42 -0600

    background color added (including rainbow!)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(function() {
   var client = filestack.init('AHP1HMkJhTJWHb1boecTAz');
 
   // upload image button
-  $('button').click(function(event) {
+  $('.uploadBtn').click(function(event) {
     event.preventDefault()
     // clear previous image
     $('#newImage').empty()
@@ -24,15 +24,28 @@ $(function() {
         url: 'https://process.filestackapi.com/AHP1HMkJhTJWHb1boecTAz/ascii=colored:true,background:black,reverse:true/' + myURL,
         type: "get",
         success: function(response) {
-          response = response.slice(53, -14)
-          response = response.replace(/font-size: 12px;/, '')
+          // remove html, head, body from returned html. remove font
+          response = response.slice(53, -14).replace(/font-size: 12px;/, '')
           console.log(response);
-          $('#newImage').append(response)
+          $('#newImage').append(response).show()
+            .attr('#red', 'newID')
         }
       })
-
       // [{"filename":"Shahada_Deshiya.jpg","handle":"xq7jrczoRlmiqbYfA8bK","mimetype":"image/jpeg","originalPath":"http://ken.uno/images/Shahada_Deshiya.jpg","size":6131189,"source":"url","url":"https://cdn.filestackcontent.com/xq7jrczoRlmiqbYfA8bK"}]
-
     })
   })
+
+  // background-color class add/subtract
+  $('.dropdown-menu').click(function() {
+    currentColor = event.target.classList
+    console.log(currentColor[1])
+    let bgBtn = $(".bgColorBtn")
+    console.log(bgBtn)
+    bgBtn.removeClass("red orange yellow green blue indigo violet black white rainbow")
+    bgBtn.addClass(currentColor[1])
+    $("div.newImage").removeClass("red orange yellow green blue indigo violet black white rainbow")
+    $("div.newImage").addClass(currentColor[1])
+  })
 })
+
+// })
